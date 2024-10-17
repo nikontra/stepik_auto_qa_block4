4625c0e6f739bbea7d6b488bf6453b85fe28fe32
nikontra <nikontra@yandex.ru>
2024-10-17 06:38:26 +0300

    create base_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
 from selenium.webdriver.common.by import By
+
+
 LINK = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
     browser.get(LINK)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    go_to_login_page(browser)
